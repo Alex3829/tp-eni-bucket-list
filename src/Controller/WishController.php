ef12165c57097117b3cc1f3bea49d6468ec84046
Alexandra <122297282+Alex3829@users.noreply.github.com>
2025-09-10 14:07:45 +0200

    TP Module 05 Données

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,22 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/wish', name: 'wish_')]
 final class WishController extends AbstractController
 {
     #[Route('/list', name: "list", methods: ['GET'])]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig');
+        $wishes = $wishRepository->findAll();
+        return $this->render(
+            'wish/list.html.twig',
+            ['wishes' => $wishes]
+        );
     }
 
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function detail(int $id): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
-        return $this->render('wish/detail.html.twig');
+        $wish = $wishRepository->find($id);
+                return $this->render(
+            'wish/detail.html.twig',
+            ['wish' => $wish]
+        );
     }
 }
