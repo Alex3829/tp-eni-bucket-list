bf4b8f53cd4fb3916079674c7517b64c0f373449
Alexandra <a.debonnes@orange.fr>
2025-09-17 14:46:24 +0200

    Reset password

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -10,23 +10,31 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
         $category1 = new Category();
-        $category1->setName('Sport');
+        $category1->setName('Travel & Adventure');
         $manager->persist($category1);
         $this->addReference('category1', $category1);
 
         $category2 = new Category();
-        $category2->setName('Activités manuelles');
+        $category2->setName('Sport');
         $manager->persist($category2);
         $this->addReference('category2', $category2);
 
         $category3 = new Category();
-        $category3->setName('Apprentissage');
+        $category3->setName('Entertainment');
         $manager->persist($category3);
         $this->addReference('category3', $category3);
+
+
+        $category4 = new Category();
+        $category4->setName('Human Relation');
+        $manager->persist($category4);
+        $this->addReference('category4', $category4);
+
+        $category5 = new Category();
+        $category5->setName('Others');
+        $manager->persist($category5);
+        $this->addReference('category5', $category5);
 
 
         $manager->flush();
---
+++ src/DataFixtures/WishFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\User;
+use App\Entity\Wish;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class WishFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $categories = $manager->getRepository(Category::class)->findAll();
+        $users = $manager->getRepository(User::class)->findAll();
+
+        for ($i = 0; $i <= 10; $i++) {
+            $wish = new Wish();
+            $wish->setTitle($faker->word);
+            $wish->setUser($faker->randomElement($users));
+            $wish->setDescription($faker->realText());
+            $wish->setDateCreated(\DateTimeImmutable::createFromMutable(
+                $faker->dateTimeBetween('-3 months', 'now')
+            ));
+            $wish->setIsPublished($faker->numberBetween(0, 1));
+            $wish->setCategory($faker->randomElement($categories));
+            $manager->persist($wish);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [CategoryFixtures::class, UserFixtures::class];
+    }
+}
